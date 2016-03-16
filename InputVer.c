109f6db3c5b31f614b3bd91ba0ca5d00b7c264df
krobison21 <katyrobison21@gmail.com>
2016-03-16 09:40:30 -0500

    Update InputVer.c

--- InputVer.c
+++ InputVer.c
@@ -2,37 +2,53 @@
 #include <string.h>
 
 int main() {
+//**************************** Open the file ********************************
 	FILE *f; //create file pointer
 	f = fopen("sudokutest.txt", "r"); //open file
 	if (f == NULL) {
 		printf("Error: File not found\n");
 		fclose(f);
 	}
-	
+//***************************************************************************	
+	int matSize = 4;
 	char str[1000];
 	char S[1000];
 	while (fgets(str, 1000, f) != NULL) {
 		//printf("%s",str);
-		strcat(S, str);
-	}
-	//int array[4] = {(int col1[4]) (int col2[4]) (int col3[4]) (int col4[4])};
-	int array[4][4];
-	int i = 0;
-	int j = 0;
-	for (int k=0; i<strlen(S);i++) {
-		if (S[k] == '\n') {
-			i++;
-			j=0;
-		}
-		if (S[k] != ' '){
-			array[i][j] = S[k];
-			j++;	
-		} 	
+		strcat(S, str); //collect input into a string
 	}
 	printf("%s\n",S);
-	for (int i=0;i<strlen(array);i++) {
-		printf(array[i]);	
+	printf("length of S: %lu\n",strlen(S));
+	printf("\n");
+
+	int array[matSize][matSize];
+	printf("a\n");
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	while (S[k] != NULL) { //collect numbers into a matrix
+		printf("S[k] = %c\n",S[k]);
+		if ((S[k] == '\n') && (S[k+1]!='\n')) {	
+			i++; //problem bc have two end of line chars (i is being updated to 3 when it should be 2
+			j=0;
+			printf("i++; j=0\n");
+		}
+		else if ((S[k] != ' ') && (S[k] != '\n')){
+			printf("Add to mat\n");
+			array[i][j] = (int)S[k];
+			j++;	
+		}
+		printf("update k\n"); 
+		k++;	
 	}
-	
+	printf("%d\n",array[0][0]); //for some reason this is printing 51
+	printf("b");
+	//Print the matrix
+	for (int a = 0; a<matSize; a++) {
+		for (int b=0; b<matSize; b++) {
+			printf("%d",array[i][j]);
+		}
+		printf("\n");
+	}
 	fclose(f);
 }

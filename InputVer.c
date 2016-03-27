165b6f27d689d1d3369a5711a837118f9e00c361
Christopher Miller <homerunhittin@gmail.com>
2016-03-27 10:33:43 -0500

    back to status quo: input(), verify(), output()

--- InputVer.c
+++ InputVer.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+// Input
+//  -Open file
+//  -Read file to string
+//  -Convert string to matrix
 void input(int matSize, int array[matSize][matSize], char *filename) {
 	//**************************** Open the file ********************************
 	FILE *f; //create file pointer
-	f = fopen("notvalidex.txt", "r"); //open file
+	f = fopen(filename, "r"); //open file
 	if (f == NULL) {
 		printf("Error: File not found\n");
 		fclose(f);
@@ -18,10 +21,161 @@ void input(int matSize, int array[matSize][matSize], char *filename) {
 	while (fgets(str, 12, f) != NULL) {
 		strcat(S, str); //collect input into a string
 	}
-	//printf("%s\n",S);
+	
+	int i = 0; // row iterator
+	int j = 0; // col iterator
+	int k = 0; // char array iterator
+	char out[1000];
+	
+	while (S[k] != '\0') { //collect numbers into a matrix
+		if ((S[k] == '\n') && (S[k+1]!='\n')) {	// reset to beginning of next row
+			i++; 
+			j=0;
+		}
+		else if ((S[k] != ' ') && (S[k] != '\n')){ 
+			array[i][j] = S[k] - '0'; // add int value to matrix
+			j++;	
+		}
+		k++;	
+	}
+	
+	fclose(f);
+	return;
+}
 
+// Check specified row for non-zero, non-repeating
+// return 0 for invalid
+// return 1 for valid
+int is_row_valid(int matSize, int array[matSize][matSize], int row) {
+	for(int i=0;i<matSize;i++) {
+		for (int j=i+1;j<matSize;j++) {
+			if (array[row][i] == 0) {
+				return 0;
+			}
+			if (array[row][i] == array[row][j]) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+	
+}
+
+// Check specified col for non-zero, non-repeating
+// return 0 for invalid
+// return 1 for valid
+int is_col_valid(int matSize, int array[matSize][matSize], int col) {
+	for(int i=0;i<matSize;i++) {
+		for (int j=i+1;j<matSize;j++) {
+			if (array[i][col] == 0) {
+				return 0;
+			}
+			if (array[i][col] == array[j][col]) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+	
+}
+
+// Check specified block for non-zero, non-repeating
+// return 0 for invalid
+// return 1 for valid
+int is_block_valid(int matSize, int array[matSize][matSize], int block) {
+	
+	int tmpArray[matSize];
+	if (block == 1) { // top left
+		int count = 0;
+		for (int i = 0; i < 3; i++) { //reformat into an array
+			for (int j = 0; j < 3; j++) {
+				tmpArray[count] = array[i][j];
+				count++;
+			}
+		}
+		for (int m = 0; m < matSize; m++) { //check validity
+			for (int n = m + 1; n < matSize; n++) {
+				if (tmpArray[m] == 0) {
+					return 0;
+				} else if (tmpArray[m] == tmpArray[n]) {
+					return 0;
+				}
+			}
+		}
+	} else if (block == 2) { // top right
+		int count = 0;
+		for (int i = 0; i < 3; i++) { //reformat into an array
+			for (int j = 6; j < 9; j++) {
+				tmpArray[count] = array[i][j];
+				count++;
+			}
+		}
+		for (int m = 0; m < matSize; m++) { //check validity
+			for (int n = m + 1; n < matSize; n++) {
+				if (tmpArray[m] == 0) {
+					return 0;
+				} else if (tmpArray[m] == tmpArray[n]) {
+					return 0;
+				}
+			}
+		}
+	} else if (block == 3) { // bottom left
+		int count = 0;
+		for (int i = 6; i < 9; i++) { //reformat into an array
+			for (int j = 0; j < 3; j++) {
+				tmpArray[count] = array[i][j];
+				count++;
+			}
+		}
+		for (int m = 0; m < matSize; m++) { //check validity
+			for (int n = m + 1; n < matSize; n++) {
+				if (tmpArray[m] == 0) {
+					return 0;
+				} else if (tmpArray[m] == tmpArray[n]) {
+					return 0;
+				}
+			}
+		}
+	} else if (block == 4) { // bottom right
+		int count = 0;
+		for (int i = 6; i < 9; i++) { //reformat into an array
+			for (int j = 6; j < 9; j++) {
+				tmpArray[count] = array[i][j];
+				count++;
+			}
+		}
+		for (int m = 0; m < matSize; m++) { //check validity
+			for (int n = m + 1; n < matSize; n++) {
+				if (tmpArray[m] == 0) {
+					return 0;
+				} else if (tmpArray[m] == tmpArray[n]) {
+					return 0;
+				}
+			}
+		}
+	}
+	return 1;
+}
+
+// Verify sudoku
+// return 0 if not valid
+// return 1 if valid
+int verify(int matSize, int array[matSize][matSize]) {
+	for (int itr = 0; itr < matSize; itr++) { //check rows & cols
+		if (~(is_row_valid(matSize,array,itr) || ~(is_col_valid(matSize,array,itr)) {
+			return 0;
+		}
+	}
+	for (int blck = 1; blck <= 4; blck++) { //check blocks
+		if (~(is_block_valid(matSize,array,blck))) {
+			return 0;
+		}
+	}
+	return 1;
+}
+	
 //Print the matrix in raw form
-void printMat(int array[matSize][matSize], int matSize) {
+void printMat(int matSize, int array[matSize][matSize]) {
 	printf("printing matrix\n");
 	for (int a = 0; a<matSize; a++) {
 		for (int b=0; b<matSize; b++) {
@@ -32,62 +186,20 @@ void printMat(int array[matSize][matSize], int matSize) {
 	}
 }
 
-int is_row_valid(int row[]) {
-	for(int i=0;i<9;i++) {
-		for (int j=i+1;j<9;j++) {
-			if (row[i] ==0) {
-				return 0;
-			}
-			if (row[i] == row[j]) {
-				return 0;
-			}
-		}
-	}
-	return 1;
-	
-}
+// Print matrix in pretty form
+void output(int matSize, int array[matSize][matSize], char *filename) {
 
-int is_col_valid(int col[]) {
-	return 1;
-}
-
-int is_block_valid(int block[]) {
-	return 1;
-}
-	
-int main() {
-
-
-	int array[matSize][matSize];
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	char out[1000];
-	
-	while (S[k] != '\0') { //collect numbers into a matrix
-		if ((S[k] == '\n') && (S[k+1]!='\n')) {	
-			i++; 
-			j=0;
-		}
-		else if ((S[k] != ' ') && (S[k] != '\n')){
-			array[i][j] = S[k] - '0';
-			j++;	
-		}
-		k++;	
-	}
-	
-	printMat(array, matSize);
-	
 	//Need to change the c==0,2 and d==0,2,3 to automatically change depending on input size
-	printf("FIX ME: print input file name\n");
-	for (int c = 0; c<matSize; c++) {
+		// Does it need to depend on size? Can't we just assume a 9x9 sudoku? -CM 3/27/2016
+	printf("%s\n",filename);
+	for (int c = 0; c < matSize; c++) { // iterate through rows
 		if (c==0)  {
 			printf("-------------------------\n");
 		}
 		else if ((c==3) || (c==6) ) {
-			printf("------------+------------\n");
+			printf("--------+--------+--------\n");
 		}
-		for (int d=0; d<matSize; d++) {
+		for (int d=0; d<matSize; d++) { // iterate through cols
 			if ((d==0) || (d==3) || (d==6) ) { //printf | before the numbers
 				printf("| ");
 				if (array[c][d]==0) {
@@ -119,11 +231,29 @@ int main() {
 			printf("-------------------------\n");	
 		}
 	}
+}
+	
+int main() {
+	int matSize = 9;
+	int sudokuArray[matSize][matSize];
+	
+	char filename[50]; // filename to open
+	printf("Input file to open: ");
+	scanf("%s",&filename);
+	
+	input(matSize,sudokuArray,filename);
+	printMat(matSize, sudokuArray);
+	
+	printf("is block 1 valid? %d\n",is_block_valid(matSize,sudokuArray,1));
+	printf("is block 2 valid? %d\n",is_block_valid(matSize,sudokuArray,2));
+	printf("is block 3 valid? %d\n",is_block_valid(matSize,sudokuArray,3));
+	printf("is block 4 valid? %d\n",is_block_valid(matSize,sudokuArray,4));
+	printf("is sudoku valid? %d\n",verify(matSize,sudokuArray));
+	output(matSize,sudokuArray,filename);
+	
 	
 	printf("array[0]: %p\n",array[0]);
 	printf("size of array: %d\n", sizeof(array)/sizeof(array[0]));
-	int x=is_row_valid(array[0]);
-	printf("is row valid? %d\n",x);
 	
-	fclose(f);
+
 }
